e8fc011d8e42f8b377b310754fcd36fe7d25b0a9
Antonis Christofides <antonis@wikical.com>
2013-08-19 10:20:21 +0300

    Export dickinson_version

---
+++ src/misc.c
@@ -0,0 +1,21 @@
+/*
+ * openmeteo.org
+ * dickinson library
+ * misc.c - miscellaneous stuff
+ *
+ * Copyright (c) 2013 Antonis Christofides
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "platform.h"
+
+const DLLEXPORT char *dickinson_version = "dev";
